c511ece2067d4f3f8aaa75468aa704438a612ace
Dev Fabrice <fabricembarush5@gmail.com>
2026-09-15 13:06:42 +0200

    Add executable linear algebra script: 11-the_western_exchange.py

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module to transpose a numpy.ndarray."""
+
+
+def np_transpose(matrix):
+    """Returns transpose of ndarray.
+    Args:
+        matrix (numpy.ndarray): the ndarray
+    Returns:
+        numpy.ndarray: transpose
+    """
+    # use transpose method
+    return matrix.transpose()
