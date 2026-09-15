efa70c93fc9a0114c8ca24a31c5177a041753078
Dev Fabrice <fabricembarush5@gmail.com>
2026-09-15 13:07:25 +0200

    Add executable linear algebra script: 6-howdy_partner.py

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module to concatenate two arrays."""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenates two arrays.
+    Args:
+        arr1 (list): first array
+        arr2 (list): second array
+    Returns:
+        list: new concatenated list
+    """
+    # concat with plus
+    return arr1 + arr2
