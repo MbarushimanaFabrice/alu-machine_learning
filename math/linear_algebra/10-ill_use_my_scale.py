9f2b5b1dbff58526d80e05cc8a600c90789a77ba
Dev Fabrice <fabricembarush5@gmail.com>
2026-09-15 13:06:36 +0200

    Add executable linear algebra script: 10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module to calculate shape of numpy.ndarray."""
+
+
+def np_shape(matrix):
+    """Returns shape of ndarray.
+    Args:
+        matrix (numpy.ndarray): the ndarray
+    Returns:
+        tuple: shape
+    """
+    # use shape attribute
+    return matrix.shape
